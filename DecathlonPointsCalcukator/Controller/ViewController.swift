34ee804556fb53af58d60b0230ae187bcc73fd52
pbasalygo <p.basalygo@student.po.edu.pl>
2022-08-19 11:57:55 +0200

    First commit : Basic app navigation created

---
+++ DecathlonPointsCalcukator/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DecathlonPointsCalcukator
+//
+//  Created by Paweł Basałygo on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var DecathlonButton: UIButton!
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
